728a5854b60dc9db9b6d61c59428b5a66f972ff1
Amanda Slayton <alslayton86@gmail.com>
2018-04-02 21:25:59 -0500

    populate superheroes

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,1 +1,30 @@
 console.log("superheroes.");
+const printToDom = (myInnerds, myElement) => {
+  document.getElementById(myElement).innerHTML = myInnerds;
+};
+
+const buildDomString = (myDataArray) => {
+  let myString = '';
+  myDataArray.forEach(element => {
+    myString += `<h4>${element.name}</h4>`;
+  });
+  printToDom(myString, "mySuperhero");
+};
+
+function executeThisFunctionAfterFileLoaded(){
+  const myData = JSON.parse(this.responseText);
+  buildDomString(myData.superheroes);
+};
+
+function ifItFails(){
+  console.log("Mistakes have been made.");
+};
+
+const startApplication = () => {
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisFunctionAfterFileLoaded);
+  myRequest.addEventListener("error",ifItFails);
+  myRequest.open("GET", "/db/superheroes.json");
+  myRequest.send();
+};
+startApplication();
