437a750af87101399f4d734689c6e2a9aa1e56fb
Amanda Slayton <alslayton86@gmail.com>
2018-04-03 20:01:10 -0500

    added dropdown to jobs page

--- javascripts/jobs.js
+++ javascripts/jobs.js
@@ -1,1 +1,31 @@
-console.log("jobs");
+const printToDom = (myInnerds, myElement) => {
+  document.getElementById(myElement).innerHTML = myInnerds;
+};
+
+const buildDomString = (heroes) => {
+  let myString = '';
+  for(let i =0; i < heroes.length; i++){
+    myString += `<li>`;
+    myString +=   `<a href ="#">${heroes[i].name}</a>`
+    myString += `</li>`;
+  };
+  printToDom(myString, "awesome-dropdown");
+};
+
+function executeThisFunctionAfterFileLoaded(){
+  const myData = JSON.parse(this.responseText);
+  buildDomString(myData.superheroes);
+};
+
+function ifItFails(){
+  console.log("Mistakes have been made.");
+};
+
+const startApplication = () => {
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisFunctionAfterFileLoaded);
+  myRequest.addEventListener("error",ifItFails);
+  myRequest.open("GET", "/db/superheroes.json");
+  myRequest.send();
+};
+startApplication();
